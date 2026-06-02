50046f52539c9c460e5342ef4176f4d93766a300
TRESTEVOYCE <hitsugayasubaru@gmail.com>
2026-06-02 07:52:13 -0700

    added CRUD for Portfolio,Publications

--- research_repo/views.py
+++ research_repo/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth import login, authenticate, logout
-from .forms import UserRegisterForm, PortfolioForm, PublicationForm, ReviewerAssignmentForm,LoginForm
-from .models import User, Portfolio, Publication, ReviewerAssignment
-from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
+from .forms import UserRegisterForm, PortfolioForm, PublicationForm,LoginForm
+from .models import User, Portfolio, Publication, Authorship, AccessGrant
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView,TemplateView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin,PermissionRequiredMixin
+from cloudinary.uploader import upload
 
 
 
@@ -46,7 +47,7 @@ class PortfolioCreateView(LoginRequiredMixin, CreateView,PermissionRequiredMixin
         return super().form_valid(form)
     
     def test_func(self):
-        return self.request.user.is_authenticated and self.request.user.role in ['FACULTY', 'PEER_REVIEWER']
+        return self.request.user.is_authenticated and self.request.user.role in ['FACULTY']
     
 class PortfolioDetailView(LoginRequiredMixin, DetailView,UserPassesTestMixin):
     model = Portfolio
@@ -54,4 +55,126 @@ class PortfolioDetailView(LoginRequiredMixin, DetailView,UserPassesTestMixin):
     context_object_name = 'portfolio'
 
     def test_func(self):
-        return self.request.user.is_authenticated and self.request.user.role in ['FACULTY', 'PEER_REVIEWER']
+        return self.request.user.is_authenticated and self.request.user.role in ['FACULTY']
+    
+class PortfolioListView(LoginRequiredMixin, ListView,UserPassesTestMixin):
+    model = Portfolio
+    template_name = 'research_repo/portfolio_list.html'
+    context_object_name = 'portfolios'
+    paginate_by = 10
+
+    def test_func(self):
+        return self.request.user.is_authenticated and self.request.user.role in ['FACULTY']
+    
+class PortfolioUpdateView(LoginRequiredMixin, UpdateView,PermissionRequiredMixin,UserPassesTestMixin):
+    model = Portfolio
+    form_class = PortfolioForm
+    template_name = 'research_repo/portfolio_form.html'
+    success_url = '/'
+    permission_required = 'research_repo.change_portfolio'
+
+    def test_func(self):
+        portfolio = self.get_object()
+        return self.request.user.is_authenticated and self.request.user == portfolio.user and self.request.user.role in ['FACULTY']
+    
+class PortfolioDeleteView(LoginRequiredMixin, DeleteView,PermissionRequiredMixin,UserPassesTestMixin):
+    model = Portfolio
+    success_url = '/'
+    permission_required = 'research_repo.delete_portfolio'
+
+    def test_func(self):
+        portfolio = self.get_object()
+        return self.request.user.is_authenticated and self.request.user == portfolio.user and self.request.user.role in ['FACULTY']
+    
+class PublicationCreateView(LoginRequiredMixin, CreateView,PermissionRequiredMixin,UserPassesTestMixin):
+    model = Publication
+    form_class = PublicationForm
+    template_name = 'research_repo/publication_form.html'
+    success_url = '/'
+    permission_required = 'research_repo.add_publication'
+
+    def form_valid(self, form):
+       pdf_file = self.request.FILES.get('full_pdf')
+
+       if pdf_file:
+            result = upload(pdf_file,resource_type='raw', folder='publications/')
+            form.instance.full_pdf_url = result['secure_url']
+        
+       form.instance.uploader = self.request.user 
+       return super().form_valid(form)
+
+    def test_func(self):
+        return self.request.user.is_authenticated and self.request.user.role in ['FACULTY']
+    
+class PublicationDetailView(DetailView):
+    model = Publication
+    template_name = 'research_repo/publication_detail.html'
+    context_object_name = 'publication'
+
+class PublicationListView(ListView):
+    model = Publication
+    template_name = 'research_repo/publication_list.html'
+    context_object_name = 'publications'
+    paginate_by = 10
+
+class PublicationUpdateView(LoginRequiredMixin, UpdateView,PermissionRequiredMixin,UserPassesTestMixin):
+    model = Publication
+    form_class = PublicationForm
+    template_name = 'research_repo/publication_form.html'
+    success_url = '/'
+    permission_required = 'research_repo.change_publication'
+
+    def form_valid(self, form):
+       pdf_file = self.request.FILES.get('full_pdf')
+
+       if pdf_file:
+            result = upload(pdf_file,resource_type='raw', folder='publications/')
+            form.instance.full_pdf_url = result['secure_url']
+        
+       return super().form_valid(form)
+
+    def test_func(self):
+        publication = self.get_object()
+        return self.request.user.is_authenticated and self.request.user.can_modify_publication(publication)
+    
+class PublicationDeleteView(LoginRequiredMixin, DeleteView,PermissionRequiredMixin,UserPassesTestMixin):
+    model = Publication
+    success_url = '/'
+    permission_required = 'research_repo.delete_publication'
+
+    def test_func(self):
+        publication = self.get_object()
+        return self.request.user.is_authenticated and self.request.user.role in ['FACULTY']
+    
+class DashboardView(LoginRequiredMixin, ListView,UserPassesTestMixin):
+    model = Publication
+    template_name = 'research_repo/dashboard.html'
+    context_object_name = 'publications'
+
+    def get_queryset(self):
+        return Publication.objects.filter(authorship__author=self.request.user).distinct()   
+
+    def test_func(self):
+        return self.request.user.is_authenticated and self.request.user.role in ['FACULTY'] 
+    
+class SetitngsView(LoginRequiredMixin, TemplateView):
+    template_name = 'research_repo/settings.html'
+
+    def test_func(self):
+        return self.request.user.is_authenticated and self.request.user.role in ['FACULTY']
+    
+
+
+    
+
+    
+
+
+    
+
+    
+
+
+
+
+
