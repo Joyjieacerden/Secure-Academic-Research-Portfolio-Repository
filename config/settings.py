3174dcd9435747758ff344d5724aedd71ef870b4
TRESTEVOYCE <hitsugayasubaru@gmail.com>
2026-06-03 22:25:26 -0700

    added rate limiting on settings as for default throttling

--- config/settings.py
+++ config/settings.py
@@ -88,6 +88,17 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
+}
+
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
